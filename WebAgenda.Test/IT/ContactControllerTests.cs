c71a1c7408e206a795785301817391186e927f02
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2021-03-21 21:55:25 -0300

    Impl inicial dos testes de integração da api.

---
+++ WebAgenda.Test/IT/ContactControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WebAgenda.Core.DTO;
+
+namespace WebAgenda.Test.IT
+{
+    class ContactControllerTests : IntegrationTest
+    {
+        [Test]
+        public async Task GetAll_WithoutAnyNumbers_ReturnsEmptyResponse()
+        {
+            var response = await TestClient.GetAsync("https://localhost/api/Contact/");
+
+            var responseDataList = JsonConvert.DeserializeObject<IEnumerable<ContactDT>>(await response.Content.ReadAsStringAsync());
+
+            (response.StatusCode).Should().Be(HttpStatusCode.OK);
+            responseDataList.Should().BeEmpty();
+        } 
+    }
+}
---
+++ WebAgenda.Test/IntegrationTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace WebAgenda.Test.IT
+{
+    class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        public IntegrationTest()
+        {
+            WebApplicationFactory<Core.Startup> appFactory = new WebApplicationFactory<WebAgenda.Core.Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<WebAgenda.Core.Data.WebAgendaContext>));
+                        if (descriptor != null) services.Remove(descriptor);
+                        services.AddDbContext<WebAgenda.Core.Data.WebAgendaContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
